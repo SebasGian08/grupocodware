4a9b9875b63638130cad1c3b234563868c87e70e
svasquez <svasquez@montgroup.com.pe>
2026-05-06 13:48:43 -0500

    update script

--- resources/views/partials/styles.blade.php
+++ resources/views/partials/styles.blade.php
@@ -5,8 +5,18 @@
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WPTDKCWB');</script>
+})(window,document,'script','dataLayer','G-GNLZFMEFZC');</script>
 <!-- End Google Tag Manager -->
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-GNLZFMEFZC"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-GNLZFMEFZC');
+</script>
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="csrf-token" content="{{ csrf_token() }}">
