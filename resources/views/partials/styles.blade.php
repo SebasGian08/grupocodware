c8a51ed9e7b3c78da24856f8f3073426c6854605
svasquez <svasquez@montgroup.com.pe>
2026-05-07 11:29:32 -0500

    mantenimiento carga

--- resources/views/partials/styles.blade.php
+++ resources/views/partials/styles.blade.php
@@ -1,25 +1,9 @@
 <meta charset="UTF-8">
 
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','G-GNLZFMEFZC');</script>
-<!-- End Google Tag Manager -->
-
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-GNLZFMEFZC"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-GNLZFMEFZC');
-</script>
-
+<!-- Responsive -->
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="csrf-token" content="{{ csrf_token() }}">
+
+<!-- SEO -->
 <title>Grupo Codware - Facturación Electrónica, Sistemas y Desarrollo Web</title>
 
 <meta name="description"
@@ -29,13 +13,63 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     content="facturación electrónica Perú, sistema de facturación electrónica SUNAT, desarrollo web, páginas web Perú, ERP, CRM, software a medida, sistemas empresariales, marketing digital, tienda virtual, automatización de procesos">
 
 <meta name="author" content="Grupo Codware - Agencia Digital">
+<meta name="robots" content="index, follow">
+<meta name="theme-color" content="#0d6efd">
 
-<!-- Open Graph (Facebook / WhatsApp) -->
+<!-- Canonical -->
+<link rel="canonical" href="https://grupocodware.com">
+
+<!-- CSRF -->
+<meta name="csrf-token" content="{{ csrf_token() }}">
+
+<!-- Open Graph / Facebook / WhatsApp -->
 <meta property="og:title" content="Facturación Electrónica, Web y Sistemas a Medida - Grupo Codware">
+
 <meta property="og:description"
     content="Soluciones digitales: facturación electrónica SUNAT, ERP, CRM, páginas web y sistemas empresariales en Perú.">
+
 <meta property="og:image" content="{{ asset('assets/images/og-image.jpg') }}">
+
+<meta property="og:url" content="https://grupocodware.com">
 <meta property="og:type" content="website">
+<meta property="og:site_name" content="Grupo Codware">
+
+<!-- Twitter Cards -->
+<meta name="twitter:card" content="summary_large_image">
+
+<meta name="twitter:title"
+    content="Grupo Codware - Facturación Electrónica y Desarrollo Web">
+
+<meta name="twitter:description"
+    content="ERP, CRM, páginas web y sistemas empresariales modernos en Perú.">
+
+<meta name="twitter:image"
+    content="{{ asset('assets/images/og-image.jpg') }}">
+
+<!-- Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-GNLZFMEFZC"></script>
+
+<script>
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+
+gtag('js', new Date());
+gtag('config', 'G-GNLZFMEFZC');
+</script>
+
+<!-- Fonts Optimization -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+
+<link rel="preconnect"
+    href="https://fonts.gstatic.com"
+    crossorigin>
+
+<link
+    href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&family=Fira+Sans:wght@300;400;500;600;700;800;900&display=swap"
+    rel="stylesheet">
 
 <!-- CSS -->
 <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}">
@@ -43,13 +77,13 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 <link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/css/personalizado.css') }}">
 
-<!-- Fonts -->
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-
-<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@300;400;500;600;700;800;900&display=swap"
-    rel="stylesheet">
-
 <!-- Favicon -->
-<link rel="icon" href="{{ asset('assets/images/favicon.png') }}">
+<link rel="icon" type="image/png"
+    href="{{ asset('assets/images/favicon.png') }}">
+
+<!-- Apple Devices -->
+<link rel="apple-touch-icon"
+    href="{{ asset('assets/images/favicon.png') }}">
+
+<!-- Optional Performance -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
